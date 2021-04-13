179db727fd2c4d97a42933adc2a639f1734f3306
Kailey Waal <15kwaal@gmail.com>
2021-04-13 14:08:34 -0400

    Added functionality to add new to do list item

--- app.js
+++ app.js
@@ -6,9 +6,43 @@ function toggleDarkMode() {
     let moon = "images/icon-moon.svg";
     let sun = "images/icon-sun.svg";
     let imageSource = document.getElementsByClassName("toggle__image")[0].src;
-    
+
     if(imageSource.includes(moon)) 
         document.getElementsByClassName("toggle__image")[0].src = sun;
     else 
         document.getElementsByClassName("toggle__image")[0].src = moon;
 }
+
+function Todo(content) {
+    this.status = "active",
+    this.content = content;
+}
+
+function addTodoToList(obj){
+    let li = document.createElement("li");
+
+    let span = document.createElement("span");
+    let checkbox = document.createElement("img");
+    checkbox.src = "images/icon-check.svg";
+    span.classList = "checkbox unchecked";
+    span.appendChild(checkbox);
+
+    let text = document.createTextNode(obj.content);
+    li.appendChild(span)
+    li.appendChild(text);
+    li.className = "checklist__item";
+
+    document.getElementsByClassName("checklist")[0].appendChild(li);
+}
+
+
+let input = document.getElementById("new-todo");
+input.addEventListener("keyup", function(event) {
+    if(event.key === "Enter") {
+        let newTodo = new Todo(input.value);
+        input.value = "";
+        addTodoToList(newTodo);
+        // document.getElementsByClassName("checklist").innerHTML = newTodo;
+    }
+})
+
