5e9271735d2c0434dac72e68ac7887e9b32884f7
Kailey Waal <15kwaal@gmail.com>
2021-04-13 16:21:06 -0400

    Added delete item functionality and update items remaining

--- app.js
+++ app.js
@@ -25,43 +25,62 @@ function addTodoToList(obj){
     let li = document.createElement("li");
 
     let span = document.createElement("span");
+    span.classList = "checkbox unchecked";
+
     let checkbox = document.createElement("img");
     checkbox.src = "images/icon-check.svg";
-    span.classList = "checkbox unchecked";
     span.appendChild(checkbox);
 
     let text = document.createTextNode(obj.content);
+
+    let deleteButton = document.createElement("span");
+    deleteButton.classList = "checklist__item--delete";
     li.appendChild(span)
     li.appendChild(text);
+    li.appendChild(deleteButton);
     li.className = "checklist__item";
 
     document.getElementsByClassName("checklist")[0].appendChild(li);
-}
 
+
+}
 
 let input = document.getElementById("new-todo");
 input.addEventListener("keyup", function(event) {
     if(event.key === "Enter") {
+        if (input.value.length === 0) return;
         let newTodo = new Todo(input.value);
         input.value = "";
         addTodoToList(newTodo);
+        addListeners();
+        updateItemsRemaining();
     }
 })
 
-// Add checked (complete) functionality
-addEventListener();
-
-function addEventListener() {
+// Add checked and delete listeners
+addListeners();
+function addListeners() {
     let checkboxes = document.getElementsByClassName("checkbox");
     for(let checkbox of checkboxes) {
         checkbox.addEventListener('click', toggleCheckbox);
     }
+
+    let deleteButtons = document.getElementsByClassName("checklist__item--delete");
+    for(let button of deleteButtons)
+        button.addEventListener("click", deleteItem);
+}
+
+function deleteItem() {
+    this.parentElement.remove();
+    updateItemsRemaining();
 }
 
 function toggleCheckbox(checkbox) {
+    let parent = (this.tagName === "SPAN") ? this.parentElement : this.parentElement.parentElement;
+    parent.classList.toggle("completed");
+
     //prevents you from adding class to img tag
-    let obj = checkbox.target;
-    let classes = (obj.tagName === "SPAN") ? obj.classList : obj.parentElement.classList;
+    let classes = (this.tagName === "SPAN") ? this.classList : this.parentElement.classList;
     if(classes.contains("unchecked")){
         classes.add("checked")
         classes.remove("unchecked");
@@ -70,26 +89,8 @@ function toggleCheckbox(checkbox) {
         classes.add("unchecked");
         classes.remove("checked");
     }
+    updateItemsRemaining();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// Delete Items
-
-
-
-
-
-
 
 // Filter List
 
@@ -100,3 +101,13 @@ function toggleCheckbox(checkbox) {
 
 
 // Determine number of items left
+function updateItemsRemaining() {
+    let items = document.getElementsByClassName("checklist__item");
+    items = Array.from(items);
+    items = items.filter(item => item.classList.contains("completed") === false).length;
+
+    let itemsDisplay = document.getElementsByClassName("items-left__number")[0];
+    itemsDisplay.innerHTML = items;
+}
+
+updateItemsRemaining();
